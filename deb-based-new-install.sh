dcf17521ce79b6e1d8e62625438132411c88a4d8
Michael Kudrak <mike@heyhew.net>
2021-03-02 12:30:48 -0500

    Cleaning up
    Cleaning up code to decrease the number of lines.
    Adding the ability to base the script on which version of the Distro

--- deb-based-new-install.sh
+++ deb-based-new-install.sh
@@ -11,13 +11,12 @@ clear
 # Query if they need current user to have sudo permissions
 echo "Does this user require sudo privilages to be added? yes or no"
 read permissions
-echo "Installing lsb-release"
-su - -c 'apt install -y lsb-release'
+echo "Installing lsb-release" && su - -c 'apt install -y lsb-release'
 distro=$(lsb_release -is)
+release=$(lsb_release -rs | cut -d. -f1)
 export currentuser=$(pwd | cut -d/ -f3)
 if [ $distro = Debian ]; then
-clear
-echo "Running for Debian"
+clear && echo "Running for Debian"
 # back up the newly installed sources
 echo "Backing up the sources file" && sleep 1
 su - -c 'cp /etc/apt/sources.list /etc/apt/list.sources.org'
@@ -27,61 +26,45 @@ clear && echo "Adding contrib and non-free" && sleep .5
 su - -c 'cat /etc/apt/sources.list | grep "contrib non-free" || sed -i "s/main/main contrib non-free/g" /etc/apt/sources.list'
 # update apt & upgrade & install lsb-release
 clear && echo "Updating & Upgrading apt & Installing lsb-release" && sleep .5
-su - -c 'apt update && apt -y upgrade && apt install -y lsb-release'
-clear && echo "Adding backports" && sleep .5
+su - -c 'apt update && apt -y upgrade && apt install -y lsb-release' && clear && echo "Adding backports" && sleep .5
 # makes the sources.list.d directory if it is not there
 su - -c 'mkdir -p /etc/apt/sources.list.d'
 # adds the backports for the $(lsb_release -cs){identifies the name of the version} to a file in the directory
 su - -c 'cat "deb http://deb.debian.org/debian $(lsb_release -cs)-backports main contrib non-free" >> /etc/apt/sources.list.d/backports.list'
 clear && echo "Updating & Upgrading - Again - Backports" && sleep .5
 su - -c 'apt update && apt -y upgrade && apt install -y firmware-linux firmware-misc-nonfree'
-su - -c 'apt update'
-su - -c 'apt -y upgrade'
-su - -c 'apt install -y remmina dnsutils screen'
-cd /home/${currentuser}/Downloads
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-su - -c 'apt install -y ./gitkraken-amd64.deb'
+su - -c 'apt update && apt -y upgrade && apt install -y remmina dnsutils screen'
+cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb'
 elif [ $distro = Ubuntu ]; then
-clear 
-echo "Running for Ubuntu" && echo "updating and upgrading and installing tools" && sleep 1
-# install remmina
-su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next'
-su - -c 'apt update && apt -y upgrade'
-su - -c 'apt install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
-cd /home/${currentuser}/Downloads
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-su - -c 'apt install -y ./gitkraken-amd64.deb'
+    if [ $release >= 20 ]; then
+        clear && echo "Running for Ubuntu" && echo "updating and upgrading and installing tools" && sleep 1
+        # install remmina
+        su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next'
+        su - -c 'apt update && apt -y upgrade' && su - -c 'apt install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
+        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb'
+    else
+        clear && echo "Running for Ubuntu" && echo "updating and upgrading and installing tools" && sleep 1
+        # install remmina
+        su - -c 'apt-get update && apt-get -y upgrade && apt-get install -y software-properties-common'
+        su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next' && su - -c 'apt-get update && apt-get -y upgrade' && su - -c 'apt-get install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
+        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt-get install -y ./gitkraken-amd64.deb'
+    fi
 elif [ $distro = Linuxmint ]; then
-clear 
-echo "Running for Linux Mint" && echo "updating and upgrading and installing tools" && sleep 1
+clear && echo "Running for Linux Mint" && echo "updating and upgrading and installing tools" && sleep 1
 # install remmina
-su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next'
-su - -c 'apt update && apt -y upgrade'
-su - -c 'apt install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
-cd /home/${currentuser}/Downloads
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-su - -c 'apt install -y ./gitkraken-amd64.deb'
+su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next' && su - -c 'apt update && apt -y upgrade' && su - -c 'apt install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
+cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb'
 elif [ $distro = Sparky ]; then
-clear 
-echo "Running for Sparky" && echo "updating and upgrading and installing tools" && sleep 1
+clear && echo "Running for Sparky" && echo "updating and upgrading and installing tools" && sleep 1
 # install remmina
-su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next'
-su - -c 'apt update && apt -y upgrade'
-su - -c 'apt install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
-cd /home/${currentuser}/Downloads
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-su - -c 'apt install -y ./gitkraken-amd64.deb'
+su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next' && su - -c 'apt update && apt -y upgrade' && su - -c 'apt install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
+cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb'
 else
-clear
-echo " Runing for unknown Distro" && echo "installing dnsutils and screen"
-su - -c 'apt update && apt -y upgrade && apt install -y dnsutils screen'
+clear && echo " Runing for unknown Distro" && echo "installing dnsutils and screen" && su - -c 'apt update && apt -y upgrade && apt install -y dnsutils screen'
 fi
 if [ $permission = yes ]; then
 # setting user to sudo rights
-clear
-echo "Setting sudo permissions"
-su - -c 'gpasswd -a '${currentuser}' sudo'
-sleep .5
+clear && echo "Setting sudo permissions" && su - -c 'gpasswd -a '${currentuser}' sudo' && sleep .5
 else
 clear && echo "sudo permissions were not set" && sleep .5
 fi

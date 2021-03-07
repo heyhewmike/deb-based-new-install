8b8f9b0bec3f928ab702c550333e11733254e846
Michael Kudrak <mike@heyhew.net>
2021-03-07 11:55:51 -0500

    New Tools and Comment
    Added the Zoom and Teamviewer download and install.
    Added comments to be able to better understand the code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # deb-based-new-install
 Basic script to setup any Debian based distro with my preferred tools
 screen dnsutils remmina
-gitkraken
+gitkraken teamviewer zoom
--- deb-based-new-install.sh
+++ deb-based-new-install.sh
@@ -12,10 +12,15 @@ clear
 echo "Does this user require sudo privilages to be added? yes or no"
 read permissions
 echo "Installing lsb-release" && su - -c 'apt install -y lsb-release'
+#Sets the distro name to a variable
 distro=$(lsb_release -is)
+#Sets the release name to a variable
 release=$(lsb_release -rs | cut -d. -f1)
+#Sets the current signed in user as a variable for use in paths and adding to sudo
 export currentuser=$(pwd | cut -d/ -f3)
+#sets a variable to 0 so that it can check for forked distros that provide the same distro name and release as Debian
 debianresult=0
+# Checks to see if there is a source file for Antix and MX - Both Debian based Distros
 anti= ls /etc/apt/sources.list.d/ | grep anti | cut -d. -f1
 mx= ls /etc/apt/sources.list.d/ | grep mx | cut -d. -f1
 if [ $anti=antix ]; then
@@ -26,6 +31,7 @@ if [ $mx=mx ]; then
 fi
 if [ $distro = Debian ]; then
     if [ $release >= 10]; then
+        #Checks to see if this is Debian and adds backports
         if [ $debianresult = 0 ]; then
             clear && echo "Running for Debian"
             # back up the newly installed sources
@@ -43,52 +49,60 @@ if [ $distro = Debian ]; then
             # adds the backports for the $(lsb_release -cs){identifies the name of the version} to a file in the directory
             su - -c 'cat "deb http://deb.debian.org/debian $(lsb_release -cs)-backports main contrib non-free" >> /etc/apt/sources.list.d/backports.list'
             clear && echo "Updating & Upgrading - Again - Backports" && sleep .5
+        # if this is antix or mx then it skips the backports as that is already present
         elif [ $debianresult >= 1 ]; then
             clear && echo "Running for MX or AntiX"
             su - -c 'apt update && apt -y upgrade && apt install -y lsb-release'
         fi
         su - -c 'apt update && apt -y upgrade && apt install -y firmware-linux firmware-misc-nonfree'
         su - -c 'apt update && apt -y upgrade && apt install -y remmina dnsutils screen'
-        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb'
+        # install GitKraken, Teamviewer and Zoom
+        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && wget https://zoom.us/client/latest/zoom_amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb ./teamviewer_amd64.deb ./zoom_amd64.deb'
     else
         echo 'deb http://ftp.debian.org/debian stretch-backports main' | sudo tee --append /etc/apt/sources.list.d/stretch-backports.list >> /dev/null
         su - -c 'apt-get update && apt-get -y upgrade && apt-get install -t stretch-backports remmina remmina-plugin-rdp remmina-plugin-secret remmina-plugin-spice && apt install -y dnsutils screen'
+        # install GitKraken, Teamviewer and Zoom
+        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && wget https://zoom.us/client/latest/zoom_amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb ./teamviewer_amd64.deb ./zoom_amd64.deb'
     fi
 elif [ $distro = Ubuntu ]; then
     if [ $release >= 20 ]; then
         clear && echo "Running for Ubuntu" && echo "updating and upgrading and installing tools" && sleep 1
-        # install remmina
+        # install remmina, Teamviewer, Zoom & GitKraken
         su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next'
         su - -c 'apt update && apt -y upgrade' && su - -c 'apt install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
-        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb'
+        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && wget https://zoom.us/client/latest/zoom_amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb ./teamviewer_amd64.deb ./zoom_amd64.deb'
     else
         clear && echo "Running for Ubuntu" && echo "updating and upgrading and installing tools" && sleep 1
-        # install remmina
+        # install remmina, Teamviewer, Zoom & GitKraken
+        # add the dependencies to use apt-add-repository
         su - -c 'apt-get update && apt-get -y upgrade && apt-get install -y software-properties-common'
         su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next' && su - -c 'apt-get update && apt-get -y upgrade' && su - -c 'apt-get install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
-        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt-get install -y ./gitkraken-amd64.deb'
+        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && wget https://zoom.us/client/latest/zoom_amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb ./teamviewer_amd64.deb ./zoom_amd64.deb'
     fi
 elif [ $distro = Linuxmint ]; then
 clear && echo "Running for Linux Mint" && echo "updating and upgrading and installing tools" && sleep 1
     if [ $release >= 20 ];then
-        # install remmina
+        # install remmina, Teamviewer, Zoom & GitKraken
         su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next' && su - -c 'apt update && apt -y upgrade' && su - -c 'apt install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
-        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb'
+        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && wget https://zoom.us/client/latest/zoom_amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb ./teamviewer_amd64.deb ./zoom_amd64.deb'
     else
         su - -c 'apt-get install -y software-properties-common'
-        # install remmina
+        # install remmina, Teamviewer, Zoom & GitKraken
+        # add the dependencies to use apt-add-repository
         su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next' && su - -c 'apt-get update && apt-get -y upgrade' && su - -c 'apt-get install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
-        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt-get install -y ./gitkraken-amd64.deb'
+        cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && wget https://zoom.us/client/latest/zoom_amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb ./teamviewer_amd64.deb ./zoom_amd64.deb'
     fi
 elif [ $distro = Sparky ]; then
 clear && echo "Running for Sparky" && echo "updating and upgrading and installing tools" && sleep 1
-# install remmina
+# install remmina, Teamviewer, Zoom, GitKraken
 su - -c 'apt-add-repository ppa:remmina-ppa-team/remmina-next' && su - -c 'apt update && apt -y upgrade' && su - -c 'apt install -y remmina remmina-plugin-rdp remmina-plugin-secret dnsutils screen'
-cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb'
+cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && wget https://zoom.us/client/latest/zoom_amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb ./teamviewer_amd64.deb ./zoom_amd64.deb'
 else
 clear && echo " Runing for unknown Distro" && echo "installing dnsutils and screen" && su - -c 'apt update && apt -y upgrade && apt install -y dnsutils screen'
+clear && echo "Downloading and installing GitKraken, Teamviewer and Zoom" && sleep .5
+cd /home/${currentuser}/Downloads && wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb && wget https://zoom.us/client/latest/zoom_amd64.deb && su - -c 'apt install -y ./gitkraken-amd64.deb ./teamviewer_amd64.deb ./zoom_amd64.deb'
 fi
-if [ $permission = yes ]; then
+if [ $permissions = yes ] or [ $permissions = y ] or [ $permissions = Yes ] or [ $permissions = Y ]; then
 # setting user to sudo rights
 clear && echo "Setting sudo permissions" && su - -c 'gpasswd -a '${currentuser}' sudo' && sleep .5
 else
